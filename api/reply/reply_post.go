9bfd43f0a7059632ad36ae8f04c1a8c76525fa2c
shiba30 <shina.17923@gmail.com>
2024-01-03 21:26:32 +0900

    fix:画像表示修正

--- api/reply/reply_post.go
+++ api/reply/reply_post.go
@@ -2,26 +2,30 @@ package reply
 
 import (
 	"database/sql"
+	"encoding/base64"
+	"fmt"
 	"log"
-	"net/http"
-	"strconv"
+	"os"
+	"time"
 
 	"example.com/golang_twitter/config"
 	"example.com/golang_twitter/db"
 	sqlc "example.com/golang_twitter/db/sqlc"
+	"example.com/golang_twitter/util"
 	"github.com/gin-gonic/gin"
 )
 
 type ReplyForm struct {
-	UserId  int64          `form:"user_id"`
-	Content string         `form:"content"`
-	Image   sql.NullString `form:"image,omitempty"`
-	ReplyTo int64          `form:"reply_to"`
+	ReplyTo int64  `json:"replyTo"`
+	UserId  int64  `json:"userId"`
+	Content string `json:"content"`
+	Image   string `json:"image,omitempty"`
 }
 
 func postReply(cfg config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		form := ReplyForm{}
+		dir := cfg.UploadedImagesDir
 
 		// リクエストデータの確認
 		if err := c.ShouldBind(&form); err != nil {
@@ -30,20 +34,69 @@ func postReply(cfg config.Config) gin.HandlerFunc {
 			return
 		}
 
-		// ツイートIDの取得
-		tweetId, err := strconv.ParseInt(c.Param("id"), 10, 64)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid tweet ID"})
+		// 140文字以内の制限
+		if len(form.Content) > 140 {
+			log.Printf("exceeds 140 characters: %v", form.Content)
+			c.JSON(400, gin.H{"error": "ツイートは140文字以内にしてください"})
 			return
 		}
+
+		// セッションからユーザー情報を取得
+		sessionID, err := c.Cookie("session_id")
+		if err != nil {
+			log.Printf("Failed to retrieve session ID: %v", err)
+			c.JSON(500, gin.H{"error": "セッションIDの取得に失敗しました"})
+			return
+		}
+		userId, err := util.GetSessionUserId(c, sessionID)
+		if err != nil {
+			log.Printf("Failed to retrieve userId from session: %v", err)
+			c.JSON(500, gin.H{"error": "セッションからユーザー情報の取得に失敗しました"})
+			return
+		}
+
+		// Base64画像データのデコード (もし画像がある場合)
+		var imagePath sql.NullString
+		if form.Image != "" {
+			var decodeErr error
+			imageData, decodeErr := base64.StdEncoding.DecodeString(form.Image)
+			if decodeErr != nil {
+				log.Printf("failed to decode base64 image data: %v", decodeErr)
+				c.JSON(400, gin.H{"error": "画像のデコードに失敗しました"})
+				return
+			}
+			if _, err := os.Stat(dir); os.IsNotExist(err) {
+				// ディレクトリが存在しない場合、作成します
+				os.MkdirAll(dir, 0755)
+			}
+			// 現在のタイムスタンプを使用して、一意のファイル名を生成
+			filename := fmt.Sprintf("%d_%d.png", time.Now().Unix(), form.UserId)
+			path := dir + filename
+			imagePath = sql.NullString{String: path, Valid: true}
+
+			// 画像データをローカルのディレクトリに保存
+			err := os.WriteFile(path, imageData, 0644)
+			if err != nil {
+				log.Printf("failed to save image data to local file: %v", err)
+				c.JSON(500, gin.H{"error": "画像の保存に失敗しました"})
+				return
+			}
+		} else {
+			imagePath = sql.NullString{Valid: false}
+		}
+
+		log.Println(newNullInt64(form.ReplyTo))
+		log.Println(userId)
+		log.Println(form.Content)
+		log.Println(imagePath)
 
 		// コメントデータの保存処理
 		queries := sqlc.New(db.DbConn())
 		_, err = queries.InsertReply(c, sqlc.InsertReplyParams{
-			UserID:    form.UserId,
+			ReplyTo:   newNullInt64(form.ReplyTo),
+			UserID:    userId,
 			Content:   form.Content,
-			ImagePath: form.Image,
-			ReplyTo:   newNullInt64(tweetId),
+			ImagePath: imagePath,
 		})
 		if err != nil {
 			log.Printf("failed to save reply: %v", err)
@@ -51,7 +104,7 @@ func postReply(cfg config.Config) gin.HandlerFunc {
 			return
 		}
 
-		log.Println("successful commnet")
+		log.Println("successful reply")
 
 		// 成功レスポンス
 		c.JSON(200, gin.H{"message": "Comment posted successfully"})
@@ -59,5 +112,8 @@ func postReply(cfg config.Config) gin.HandlerFunc {
 }
 
 func newNullInt64(i int64) sql.NullInt64 {
-	return sql.NullInt64{Int64: i, Valid: i != 0}
+	if i == 0 {
+		return sql.NullInt64{Valid: false}
+	}
+	return sql.NullInt64{Int64: i, Valid: true}
 }
--- static/js/tweet_detail.js
+++ static/js/tweet_detail.js
@@ -1,69 +1,115 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const tweetId = window.location.pathname.split("/").pop();
-  const userId = replyForm.getAttribute("data-user-id");
+  const replyForm = document.getElementById("replyForm");
+  const tweetId = parseInt(window.location.pathname.split("/").pop(), 10);
+  const userId = parseInt(replyForm.getAttribute("data-user-id"), 10);
+  const replyContent = document.getElementById("replyContent");
+  const uploadImage = document.getElementById("uploadImage");
+  const imagePreview = document.getElementById("selectedImagePreview");
+
+  // 画像プレビュー
+  uploadImage.addEventListener("change", function () {
+    const file = uploadImage.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        imagePreview.src = e.target.result;
+        imagePreview.style.display = "block";
+      };
+      reader.onerror = function (error) {
+        console.error("Error reading the file:", error);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      imagePreview.style.display = "none";
+    }
+  });
 
   // 返信（リプライ）を投稿する機能
-  document.getElementById("postReply").addEventListener("click", function () {
-    const replyContent = document.getElementById("replyContent").value;
-    const uploadImage = document.getElementById("uploadImage").files[0];
+  replyForm.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    if (!replyContent) {
-      alert("Please enter a reply");
-      return;
+    let formData = {
+      replyTo: tweetId,
+      userId: userId,
+      content: replyContent.value,
+    };
+
+    // 画像をBase64としてエンコード
+    const file = uploadImage.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        formData.image = reader.result.split(",")[1];
+
+        sendReplyRequest(formData);
+      };
+      reader.onerror = function (error) {
+        console.error("Error reading the file:", error);
+      };
+    } else {
+      sendReplyRequest(formData);
     }
+  });
 
-    const formData = new FormData();
-    formData.append("user_id", userId);
-    formData.append("content", replyContent);
-    formData.append("reply_to", tweetId);
-
-    if (uploadImage) {
-      formData.append("image", uploadImage);
-    }
-
+  function sendReplyRequest(formData) {
     fetch(`/reply/${tweetId}`, {
       method: "POST",
-      body: formData, // ここでFormDataを使用
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Reply post failed");
+          return response.json().then((err) => {
+            throw err;
+          });
         }
         return response.json();
       })
-      .then(() => {
-        alert("リプライ完了");
-        document.getElementById("replyContent").value = "";
-        if (uploadImage) {
-          document.getElementById("selectedImagePreview").src = "";
-          document.getElementById("selectedImagePreview").style.display =
-            "none";
+      .then((data) => {
+        if (data.message === "Comment posted successfully") {
+          alert("リプライ完了");
+          replyContent.value = "";
+          imagePreview.src = "";
+          imagePreview.style.display = "none";
+          loadReplies(); // 返信を再読み込み
+        } else {
+          alert("リプライ失敗");
         }
-        loadReplies(); // 返信を再読み込み
       })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("リプライ失敗");
+      .catch((errorData) => {
+        if (errorData && errorData.error) {
+          alert(errorData.error);
+        } else {
+          console.error("Error:", errorData);
+          alert("内部エラー");
+        }
       });
-  });
+  }
 
   // 返信（リプライ）を読み込む機能
   function loadReplies() {
     fetch(`/reply/replies/${tweetId}`)
       .then((response) => response.json())
       .then((data) => {
+        const avatarImage = "/static/img/default_avatar.png";
         const repliesListDiv = document.getElementById("repliesList");
         repliesListDiv.innerHTML = "";
+
         data.replies.forEach((reply) => {
           const replyElement = document.createElement("div");
-          replyElement.className = "border-bottom d-flex flex-row m-3 reply";
+          replyElement.className = "border-bottom d-flex flex-row m-3 tweet";
+          replyElement.setAttribute("data-tweet-id", reply.tweet_id); // ツイートIDを設定
 
-          // アバター画像の表示
           const avatarDiv = document.createElement("div");
           avatarDiv.className = "pr-3";
 
           const userImage =
-            reply.user_image.String || "/static/img/default_avatar.png";
+            reply.user_image && reply.user_image.Valid
+              ? reply.user_image.String
+              : avatarImage;
           const avatarImg = document.createElement("img");
           avatarImg.src = userImage;
           avatarImg.width = 50;
@@ -72,13 +118,11 @@ document.addEventListener("DOMContentLoaded", function () {
           avatarDiv.appendChild(avatarImg);
           replyElement.appendChild(avatarDiv);
 
-          // コンテンツ部分の表示
           const contentDiv = document.createElement("div");
-
-          // ユーザー名とリプライ日時の表示
           const userNameDiv = document.createElement("div");
           userNameDiv.innerText = reply.display_name;
-          userNameDiv.style.marginBottom = "5px";
+          userNameDiv.style.marginBottom = "15px";
+
           const createdAtText = document.createElement("small");
           createdAtText.innerText = `　- ${new Date(
             reply.tweet_date
@@ -86,7 +130,6 @@ document.addEventListener("DOMContentLoaded", function () {
           createdAtText.className = "text-muted";
           userNameDiv.appendChild(createdAtText);
 
-          // リプライテキストの表示
           const replyContentDiv = document.createElement("div");
           replyContentDiv.innerText = reply.tweet_content;
           replyContentDiv.style.marginBottom = "15px";
@@ -94,22 +137,24 @@ document.addEventListener("DOMContentLoaded", function () {
           contentDiv.appendChild(userNameDiv);
           contentDiv.appendChild(replyContentDiv);
 
-          // リプライに画像が含まれている場合の表示
           if (reply.image_path && reply.image_path.Valid) {
             const imageContainer = document.createElement("div");
             imageContainer.className = "justify-content-center";
 
             const replyImage = document.createElement("img");
-            replyImage.src = reply.image_path.String;
-            replyImage.alt = "Reply image";
-            replyImage.className = "mt-2";
+            replyImage.src = "/" + reply.image_path.String;
             replyImage.style.width = "100%";
             replyImage.style.height = "100%";
             replyImage.style.objectFit = "cover";
+            replyImage.style.border = "0.2px solid #ddd";
 
             imageContainer.appendChild(replyImage);
             contentDiv.appendChild(imageContainer);
           }
+
+          const todoDiv = document.createElement("div");
+          todoDiv.className = "mb-3";
+          contentDiv.appendChild(todoDiv);
 
           replyElement.appendChild(contentDiv);
 
